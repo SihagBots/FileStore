9ea23f57d97c8885f902c8761c3814f83d02fc8c
SihagBots <sihagamit308@gmail.com>
2026-03-10 18:29:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ SHORT_TUT = "https://t.me/HowToDownloadSnap/2"
 VERIFY_COOLDOWN = int(os.environ.get("VERIFY_COOLDOWN", "180"))
 VERIFY_REDIRECT_DELAY = int(os.environ.get("VERIFY_REDIRECT_DELAY", "5"))
 VERIFY_LOG_CHANNEL = int(os.environ.get("VERIFY_LOG_CHANNEL", "0"))
-SERVICE_URL = os.environ.get("SERVICE_URL", "")
+SERVICE_URL = os.environ.get("SERVICE_URL", "https://exciting-rodent-sihagbots-e5b9a9ef.koyeb.app")
 
 # Bot Configuration
 SESSION = "SnapXPagluBot"
